09c56592ea0a9e06d4becfaca4ec28ce0270f668
Brosky <brosky_halim@bca.co.id>
2021-10-28 11:21:36 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './css/styles.css'
+import StatefullExample from './day1-practice/components/beforeHooks/statefull';
+import FetchDataHooks from './day1-practice/FetchHooks';
+import CounterApp from './day1-practice/counterApp';
+import CounterHooks from './day1-practice/Hooks/CounterHooks';
+import TransferApp from './day2-practice/components';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello Everyone</h2>
+      <TransferApp />
+    </div>
+  );
+}
+
+export default App;
